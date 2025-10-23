342d4bbcbcd329a0756c525dad1eeb223039fa30
npaarlbe <nathaniel.paarlberg@gmail.com>
2025-10-23 10:38:27 -0400

    add e14

---
+++ GA.py
@@ -0,0 +1,25 @@
+import random
+
+def main():
+    pass
+
+def createPopulation():
+    pass
+
+def fitness():
+    pass
+
+def selection():
+    pass
+
+def crossover():
+    pass
+
+main()
+# population size
+# chromosome size
+# gen bounds
+# mut rate 
+# cross rate
+# maxGens
+# 
